e1e7f8f3c81fdfe3f1fce90eec617ebefa8ff3f7
dnorris2018 <dnorris2018@yahoo.com>
2018-07-07 22:15:32 -0400

    Minecraft Mod
    First

---
+++ KnightWolfMod/src/main/java/com/Knightwolf21/KnightWolfMod/proxy/ClientProxy.java
@@ -0,0 +1,6 @@
+package com.Knightwolf21.KnightWolfMod.proxy;
+
+public class ClientProxy extends CommonProxy
+{
+	
+}
---
+++ KnightWolfMod/src/main/java/com/Knightwolf21/KnightWolfMod/util/Reference.java
@@ -0,0 +1,11 @@
+package com.Knightwolf21.KnightWolfMod.util;
+
+public class Reference 
+{
+	public static final String MOD_ID             = "kw";
+	public static final String NAME               = "KnightWolf Mod";
+	public static final String VERSION            = "1.0";
+	public static final String ACCEPTED_VERSIONS  = "[1.12.2]";
+	public static final String CLIENT_PROXY_CLASS = "com.Knightwolf21.KnightWolfMod.proxy.ClientProxy";
+	public static final String COMMON_PROXY_CLASS = "com.Knightwolf21.KnightWolfMod.proxy.CommonProxy";
+}
